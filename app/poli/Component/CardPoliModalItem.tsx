07c3d24a9512adbe2a28b0142c2aca3e4684c7cd
Amirul Azmi <amirullazmi0@gmail.com>
2023-10-26 11:10:25 +0700

    gas

--- app/poli/Component/CardPoliModalItem.tsx
+++ app/poli/Component/CardPoliModalItem.tsx
@@ -11,7 +11,7 @@ const CardPoliModalItem = ({ data }: { data: any }) => {
                     return (
                         <React.Fragment key={index}>
                             <div className="carousel-item flex gap-3 lg:w-1/4 w-full">
-                                <div className={`card bg-base-100 shadow-xl border ${data[index + 1] && `mr-3`}`}>
+                                <div className={`card bg-white shadow-xl border ${data[index + 1] && `mr-3`}`}>
                                     <figure className='flex justify-center items-center'>
                                         <img className='aspect-square w-full' src={`${item.url}`} />
                                     </figure>
@@ -23,7 +23,7 @@ const CardPoliModalItem = ({ data }: { data: any }) => {
                                     <div className="p-4 grid gap-2">
                                         {item.jadwal && item.jadwal.map((item2: any, index: number) => {
                                             return (
-                                                <div key={index} className="grid grid-cols-5 rounded-lg bg-base-100 text-slate-950 p-2">
+                                                <div key={index} className="grid grid-cols-5 rounded-lg bg-white text-slate-950 p-2">
                                                     <div className="col-span-2 flex items-center gap-1">
                                                         <div className="text-xs">
                                                             {item2.hari_kerja}
